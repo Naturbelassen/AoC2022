4fcd60392c392d3c4afdcfb951e72facb020a11c
WT <C:/Program Files/Git/>
2022-12-01 17:51:45 +0100

    Updated comments

--- Day1.java
+++ Day1.java
@@ -8,13 +8,12 @@ import java.util.List;
 public class Day1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int max = 0;
 		int placeHolder = 0;
 		try {
 			List<String> myMap = Files.readAllLines(Paths.get("Path_To_Your_File")); //Add the Path to your File
 			//Add all Numbers until \n is found
-			//Check if plsceHolder is greater than max
+			//Check if placeHolder is greater than max
 			//Reset placeHolder to 0 and add again until \n is found
 			for(String myString : myMap) {
 				if(myString.length() != 0) {
