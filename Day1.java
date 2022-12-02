e9c5a43f5141f7d24ab989be475d110ec73296ba
WT <C:/Program Files/Git/>
2022-12-02 15:36:08 +0100

    Day2 of AdventOfCode 2022

--- Day1.java
+++ Day1.java
@@ -5,17 +5,21 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+/*
+*	Link to the Challenge https://adventofcode.com/2022/day/1
+*/
+
 public class Day1 {
 
 	public static void main(String[] args) {
 		int max = 0;
 		int placeHolder = 0;
 		try {
-			List<String> myMap = Files.readAllLines(Paths.get("Path_To_Your_File")); //Add the Path to your File
+			List<String> myList = Files.readAllLines(Paths.get("Path_To_Your_File")); //Add the Path to your File
 			//Add all Numbers until \n is found
 			//Check if placeHolder is greater than max
 			//Reset placeHolder to 0 and add again until \n is found
-			for(String myString : myMap) {
+			for(String myString : myList) {
 				if(myString.length() != 0) {
 					placeHolder += Integer.parseInt(myString);				
 				}else {
--- Day1_2.java
+++ Day1_2.java
@@ -6,6 +6,10 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+*	Link to the Challenge https://adventofcode.com/2022/day/1
+*/
+
 public class Day1_2 {
 
 	public static void main(String[] args) {
@@ -13,11 +17,11 @@ public class Day1_2 {
 		int placeHolder = 0;
 		int[] topThree = {0,0,0}; 
 		try {
-			List<String> myMap = Files.readAllLines(Paths.get("Path_To_Your_File")); //Add the Path to your File
+			List<String> myList = Files.readAllLines(Paths.get("Path_To_Your_File")); //Add the Path to your File
 			//Add all Numbers until \n is found
 			//Check if placeHolder is greater than any number in topThree
 			//Reset placeHolder to 0 and add again until \n is found
-			for(String myString : myMap) {
+			for(String myString : myList) {
 				if(myString.length() != 0) {
 					placeHolder += Integer.parseInt(myString);				
 				}else {
